86c935f0dda5680cc2be68899b60c866278fe2fe
Vtzinhacker <vtz.rodrigues666@gmail.com>
2025-10-27 19:50:02 -0300

    Create tipos_de_notificação.java

---
+++ tipos_de_notificação.java
@@ -0,0 +1,7 @@
+public enum TipoNotificacao {
+VERMELHO, // Notificação urgente (pagamentos, atrasos, pendências)
+AMARELO, // Notificação de atenção (alterações de horário,
+cancelamentos)
+BRANCO // Notificação informativa (confirmações e informações gerais)
+}
+
